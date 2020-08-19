fccb4187560b7427a5eac0dc62c365c2b23d7de9
Dmitry Christie <dmitry.christie@gmail.com>
2020-08-19 11:23:08 +0200

    removed toggle

--- footer.php
+++ footer.php
@@ -150,22 +150,6 @@ var menu = ['Lisbon', 'Porto', 'Braga', 'Faro', 'Coimbra', 'Evora'];
 
 </script>
 
-<script>
 
-(function($) {
-$('#toggle').toggle( 
-    function() {
-        $('#popout').animate({ left: 0 }, 'slow', function() {
-            $('#toggle').html('<img src="<?php echo get_template_directory_uri(); ?>/images/hamburger.png">');
-        });
-    }, 
-    function() {
-        $('#popout').animate({ left: -250 }, 'slow', function() {
-            $('#toggle').html('<img src="<?php echo get_template_directory_uri(); ?>/images/hamburger.png">');
-        });
-    }
-);
-})(jQuery);
-</script>
 </body>
 </html>
--- header.php
+++ header.php
@@ -104,13 +104,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 					);
 					?>
 				</nav><!-- #site-navigation -->
-				<div id="toggle">
-					<img alt="Residency and citizenship by investment" src="<?php echo get_template_directory_uri(); ?>/images/hamburger.png"></div>
-					<div id="popout">
-						<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_class' => 'nav-menu-mobile' ) ); ?>
-					</div>
-
-				</div>
+				
 		</div>
 	<hr >
 	</div>
