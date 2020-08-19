164946effd81424a9495526648aaebbaa62b453a
Dmitry Christie <dmitry.christie@gmail.com>
2020-08-19 17:38:50 +0200

    test

--- inc/flexible-content.php
+++ inc/flexible-content.php
@@ -491,7 +491,6 @@
                             </h2>
                             <?php
 							
-								the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 							
 
 							if ( 'post' === get_post_type() ) :
